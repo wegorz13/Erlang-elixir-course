9c73019d66a85263b821c2cd01e5cee4e35a0cc6
Filip <filipwegrzyn85@gmail.com>
2025-05-02 13:43:11 +0200

    lab 4 gen server

---
+++ src/lab4/pollution.erl
@@ -0,0 +1,110 @@
+-module(pollution).
+-author("filip").
+
+-export([]).
+
+-record(measurement, {type, cords, date, value}).
+
+create_monitor() -> #{}.
+
+add_station(Name, Cords, Monitor) ->
+  Key = {Name, Cords},
+  case maps:is_key(Key, Monitor) of
+    false -> Monitor#{{Name, Cords} => []};
+    true -> {error, "the station already exists"}
+  end.
+
+add_value(Identifier, Date, Type, Value, Monitor)->
+  case find_station(Identifier, maps:keys(Monitor)) of
+    {error, Msg} -> {error, Msg};
+
+    {Name, Cords} ->
+      Measurement = #measurement{type=Type, cords=Cords, date=Date, value = Value},
+      M_list = maps:get({Name, Cords}, Monitor),
+      case verify_measurements(Measurement, M_list) of
+        ok -> maps:update({Name, Cords}, [Measurement | M_list], Monitor);
+        {error, Msg} -> {error, Msg}
+      end
+  end.
+
+remove_value(Identifier, Date, Type, Monitor)->
+  case find_station(Identifier, maps:keys(Monitor)) of
+    {error, Msg} -> {error, Msg};
+
+    {Name, Cords} ->
+      Original_list = maps:get({Name, Cords}, Monitor),
+      Updated_list = lists:filter(
+        fun (#measurement{type = Type_f, cords = Cords_f, date = Date_f}) ->
+          not (Type_f == Type andalso Cords_f == Cords andalso Date_f == Date)
+        end, Original_list),
+      maps:update({Name, Cords}, Updated_list, Monitor)
+  end.
+
+get_one_value(Identifier, Date, Type, Monitor)->
+  case find_station(Identifier, maps:keys(Monitor)) of
+    {error, Msg} -> {error, Msg};
+    {Name, Cords} ->
+      Pred = fun (#measurement{type=Type_f, cords=_, date=Date_f, value = _}) -> Type_f == Type andalso Date_f == Date end,
+      case lists:search(Pred, maps:get({Name, Cords}, Monitor)) of
+        {value, M} -> M#measurement.value;
+        false -> {error, "such measurement doesnt exist"}
+      end
+  end.
+
+get_station_min(Identifier, Type, Monitor) ->
+  case find_station(Identifier, maps:keys(Monitor)) of
+    {error, Msg} -> {error, Msg};
+
+    {Name, Cords} ->
+      Compare = fun (Elem, Acc) ->
+        if
+          Acc#measurement.value < Elem#measurement.value -> Acc;
+          true -> Elem
+        end
+                end,
+
+      Type_measurements = lists:filter(fun (#measurement{type=F_type, cords=_, date=_, value=_}) -> Type==F_type end, maps:get({Name, Cords}, Monitor)),
+      if
+        Type_measurements == [] -> {error, "There are no measurements of provided type"};
+        true ->
+          Acc = lists:last(Type_measurements),
+          (lists:foldl(Compare, Acc, Type_measurements))#measurement.value
+      end
+  end.
+
+get_daily_mean(Type, Date, Monitor)->
+  Ftr_by_date_nd_type = fun (#measurement{type=F_type, cords=_, date= {F_date, _}, value=_}) -> Date == F_date andalso Type == F_type end,
+  Sum_values = fun (#measurement{type=_, cords=_, date=_, value=N_value}, Acc) -> N_value + Acc end,
+  Measurements = lists:filter(Ftr_by_date_nd_type, lists:flatten(maps:values(Monitor))),
+  case Measurements of
+    [] -> {error, "No measurements found for provided type and date"};
+    _  -> lists:foldl(Sum_values, 0, Measurements) / length(Measurements)
+  end.
+
+get_daily_average_data_count(Monitor) ->
+  Unique_days = fun (#measurement{type=_, cords=_, date= {Date, _}, value=_}, Acc) ->
+    case lists:member(Date,Acc) of
+      true -> Acc;
+      false -> [Date | Acc]
+    end end,
+  Day_average = fun (_, Measurements) ->
+    case Measurements of
+      [] -> 0;
+      _ -> length(Measurements)/length(lists:foldl(Unique_days, [], Measurements))
+    end end,
+
+  Average_per_station = maps:map(Day_average, Monitor),
+  lists:sum(maps:values(Average_per_station))/map_size(Average_per_station).
+
+find_station(Identifier, [{Identifier, Cords} | _]) ->
+  {Identifier,Cords};
+find_station(Identifier, [{Name, Identifier} | _]) ->
+  {Name, Identifier};
+find_station(Identifier, [_ | T]) ->
+  find_station(Identifier, T);
+find_station(_, []) -> {error, "the station is missing"}.
+
+verify_measurements(#measurement{type=Type, cords=Cords, date=Date, value = _}, [#measurement{type=Type, cords=Cords, date=Date, value = _} | _]) ->
+  {error, "there is a conflicting measurement in the monitor"};
+verify_measurements(Measurement, [_|T] )->verify_measurements(Measurement, T);
+verify_measurements(_, [])->ok.
---
+++ src/lab4/pollution_gen_server.erl
@@ -0,0 +1,102 @@
+-module(pollution_gen_server).
+-behaviour(gen_server).
+
+-export([add_station/2, add_value/4, remove_value/3, get_one_value/3, get_station_min/2, get_daily_mean/2, get_daily_average_data_count/0, start/1, start/0, stop/0, crash/0, init/1, handle_call/3, handle_cast/2]).
+
+
+start(InitialValue)->
+  start_link(InitialValue).
+
+start()->
+  start_link(pollution:create_monitor()).
+
+stop() ->
+  gen_server: cast(pollution_server, stop).
+
+crash()->
+  1/0.
+
+init(InitialValue) ->
+  {ok, InitialValue}.
+
+start_link(InitialValue)->
+  gen_server:start_link({local, pollution_server}, pollution_gen_server, InitialValue, []).
+
+terminate(Reason, Value) ->
+  io:format("Server: exit with value ~p~n", [Value]),
+  Reason.
+
+add_station(Name, Cords) ->
+  gen_server:cast(pollution_server, {add_station, Name, Cords}).
+
+add_value(Identifier, Date, Type, Value) ->
+  gen_server:cast(pollution_server, {add_value, Identifier, Date, Type, Value}).
+
+remove_value(Identifier, Date, Type) ->
+  gen_server:cast(pollution_server, {remove_value, Identifier, Date, Type}).
+
+get_one_value(Identifier, Date, Type) ->
+  gen_server:call(pollution_server, {get_one_value, Identifier, Date, Type}).
+
+get_station_min(Identifier, Type) ->
+  gen_server:call(pollution_server, {get_station_min, Identifier, Type}).
+
+get_daily_mean(Type, Date) ->
+  gen_server:call(pollution_server, {get_daily_mean, Type, Date}).
+
+get_daily_average_data_count() ->
+  gen_server:call(pollution_server, {get_daily_average_data_count}).
+
+
+handle_cast({add_station, Name, Cords}, State) ->
+  case pollution:add_station(Name, Cords, State) of
+    {error, Msg} ->
+      io:format("ERROR: ~s. ~n", [Msg]),
+      {noreply, State};
+    New_state ->
+      {noreply, New_state}
+  end.
+
+handle_cast({add_value, Identifier, Date, Type, Value}, State) ->
+  case pollution:add_value(Identifier, Date, Type, Value, State) of
+    {error, Msg} ->
+      io:format("ERROR: ~s. ~n", [Msg]),
+      {noreply, State};
+    New_state ->
+      {noreply, New_state}
+  end.
+
+handle_cast({remove_value, Identifier, Date, Type}, State) ->
+  case pollution:remove_value(Identifier, Date, Type, State) of
+    {error, Msg} ->
+      io:format("ERROR: ~s. ~n", [Msg]),
+      {noreply, State};
+    New_state ->
+      {noreply, New_state}
+  end.
+
+handle_cast(stop, State) ->
+  {stop, normal, State}.
+
+handle_call({get_one_value, Identifier, Date, Type}, _From, State) ->
+  Value = pollution:get_one_value(Identifier, Date, Type, State),
+  {reply, Value, State}.
+
+handle_call({get_station_min, Identifier, Type}, _From, State) ->
+  Value = pollution:get_station_min(Identifier, Type, State),
+  {reply, Value, State}.
+
+handle_call({get_daily_mean, Type, Date}, _From, State) ->
+  Value = pollution:get_daily_mean(Type, Date, State),
+  {reply, Value, State}.
+
+handle_call({get_daily_average_data_count}, _From, State) ->
+  Value = pollution:get_daily_average_data_count(State),
+  {reply, Value, State}.
+
+handle_info(UnknownMessage, State) ->
+  io:format("Server received unexpected message", [UnknownMessage]),
+  {noreply, State}.
+
+
+
